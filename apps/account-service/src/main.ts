d9ee10a547939cb91fd8c91533443b63f73e4f41
advaithsuryaP <padvaithsurya@gmail.com>
2026-02-26 18:30:14 -0500

    Update microservices setup and add @nestjs/microservices dependency
    - Added @nestjs/microservices dependency to package.json and package-lock.json.
    - Refactored account, transaction, and user services to utilize NestJS microservices architecture with TCP transport.
    - Updated service startup logic to reflect microservice configuration and removed unused global prefix settings.
    - Enhanced logging to indicate the running ports for each microservice.

--- apps/account-service/src/main.ts
+++ apps/account-service/src/main.ts
@@ -3,17 +3,25 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+// import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { MicroserviceOptions } from '@nestjs/microservices';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    const globalPrefix = 'api';
-    app.setGlobalPrefix(globalPrefix);
-    const port = process.env.PORT || 3000;
-    await app.listen(port);
-    Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+    const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+        transport: Transport.TCP,
+        options: {
+            port: 4002
+        }
+    });
+    // const globalPrefix = 'api';
+    // app.setGlobalPrefix(globalPrefix);
+    // const port = process.env.PORT || 3000;
+    await app.listen();
+    console.log('Account microservice is running on port 4002');
+    // Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
 
 bootstrap();
--- apps/user-service/src/main.ts
+++ apps/user-service/src/main.ts
@@ -3,17 +3,24 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+// import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    const globalPrefix = 'api';
-    app.setGlobalPrefix(globalPrefix);
-    const port = process.env.PORT || 3000;
-    await app.listen(port);
-    Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+    const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+        transport: Transport.TCP,
+        options: {
+            port: 4003
+        }
+    });
+    // const globalPrefix = 'api';
+    // app.setGlobalPrefix(globalPrefix);
+    // const port = process.env.PORT || 3000;
+    await app.listen();
+    console.log('User microservice is running on port 4003');
+    // Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
 
 bootstrap();
