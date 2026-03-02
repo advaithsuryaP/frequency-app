099abb48661665ba5f3e8adfb1ddf6bb17652e8d
advaithsuryaP <padvaithsurya@gmail.com>
2026-03-02 14:03:54 -0500

    Remove transaction service and related components from the project
    - Deleted the transaction service, including its controllers, services, and configuration files.
    - Removed references to the transaction service in the API Gateway and TypeScript configuration.
    - Cleaned up the tsconfig.json by eliminating unused paths related to the transaction service.
    - This refactor streamlines the microservices architecture by focusing on the remaining services.

--- apps/api-gateway/src/app/app.module.ts
+++ apps/api-gateway/src/app/app.module.ts
@@ -1,38 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { MICROSERVICES_CLIENTS } from './app.constants';
-import { TransactionsController } from '../transactions/transactions.controller';
-import { AccountsController } from '../accounts/accounts.controller';
 
 @Module({
-    imports: [
-        ClientsModule.register([
-            {
-                name: MICROSERVICES_CLIENTS.TRANSACTION_SERVICE,
-                transport: Transport.TCP,
-                options: {
-                    port: 4001
-                }
-            },
-            {
-                name: MICROSERVICES_CLIENTS.ACCOUNT_SERVICE,
-                transport: Transport.TCP,
-                options: {
-                    port: 4002
-                }
-            },
-            {
-                name: MICROSERVICES_CLIENTS.USER_SERVICE,
-                transport: Transport.TCP,
-                options: {
-                    port: 4003
-                }
-            }
-        ])
-    ],
-    controllers: [AppController, TransactionsController, AccountsController],
+    imports: [],
+    controllers: [AppController],
     providers: [AppService]
 })
 export class AppModule {}
