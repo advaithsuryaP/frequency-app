8c7335d8b88c58128e452b52388950e27a2fa303
advaithsuryaP <padvaithsurya@gmail.com>
2026-03-03 01:54:47 -0500

    Enhance Waves microservice with gRPC support and project configuration updates
    - Updated nx.json to include build options for proto assets.
    - Modified tsconfig.app.json to reference the new waves-contracts library.
    - Refactored main.ts to create a gRPC microservice using the WavesService.
    - Adjusted waves.proto to correct service definition syntax.
    - Improved type definitions in waves.ts for better consistency and readability.

--- apps/waves/src/main.ts
+++ apps/waves/src/main.ts
@@ -6,14 +6,22 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { protobufPackage } from '@frequency-app/waves-contracts';
+import { join } from 'path';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    const globalPrefix = 'api';
-    app.setGlobalPrefix(globalPrefix);
-    const port = process.env.PORT || 3000;
-    await app.listen(port);
-    Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+    const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+        transport: Transport.GRPC,
+        options: {
+            package: protobufPackage,
+            protoPath: join(__dirname, 'proto/waves.proto')
+        }
+    });
+
+    await app.listen();
+
+    Logger.log(`🚀 Waves microservice is running on gRPC channel`);
 }
 
 bootstrap();
